7f41d0d145e58e9889d29fcbd23acd60b6cf7c32
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2016-07-19 09:52:06 +0300

    Download preview table as png image
    .png extension and file name only supported on Chrome 14+ and Firefox
    20+, for other browsers the user has to change the file name

--- download script.js
+++ download script.js
@@ -1,4 +1,19 @@
-// JQuery scripts is linked in index.html
-function downloadPNG(){
-	var img = $('#preview-table').tableExport({type:'png',escape:'false'});
+var canvas = {};
+var img = new Image();
+
+function DownloadPNG(){
+	html2canvas($('#preview-table'), {
+		onrendered: function(canvas){
+			img.src = canvas.toDataURL("image/png");
+			var url = img.src.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
+			var trigger = $("<a>")
+		      .attr("href", url)
+		      .attr("download", "timetable.png")
+		      .appendTo("body");
+
+			trigger[0].click();
+
+			trigger.remove();
+		}
+	});
 }
