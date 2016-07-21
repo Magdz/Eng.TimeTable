79a20daca170d86cdd1fafab18b8390110ae9fa6
Nader Alsharkawy <subnader@gmail.com>
2016-07-21 20:41:57 +0400

    Fix download issues
    -Fixed download issues, including malformed timetables.
    -Added IE support.

--- js/download-script.js
+++ js/download-script.js
@@ -6,14 +6,20 @@ function DownloadPNG(){
 		onrendered: function(canvas){
 			img.src = canvas.toDataURL("image/png");
 			var url = img.src.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
+			if (navigator.userAgent.indexOf("MSIE ") > 0 || navigator.userAgent.match(/Trident.*rv\:11\./)) 
+			{
+				var blob = canvas.msToBlob();
+				window.navigator.msSaveBlob(blob,'timetable.png');
+			}
+			else{
 			var trigger = $("<a>")
 		      .attr("href", url)
 		      .attr("download", "timetable.png")
 		      .appendTo("body");
 
 			trigger[0].click();
-
 			trigger.remove();
+			}
 		}
 	});
 }
